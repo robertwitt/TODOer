3effda19986b4559fffcf4f8b82a46bf570fa1a0
robertwitt <robert_witt@icloud.com>
2021-04-05 16:30:46 +0200

    Setup express app

--- server/express/src/app.ts
+++ server/express/src/app.ts
@@ -1,5 +1,16 @@
-main();
+import express from "express";
+import http from "http";
+import { handleError } from "./middleware/error";
 
-function main() {
-  console.log(`Hello Typescript!`);
-}
+const app = express();
+
+// Setup body parsers
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: false }));
+
+// Add error handler
+app.use(handleError);
+
+http.createServer(app).listen(3000);
+console.log("Server started and listens to port 3000");
