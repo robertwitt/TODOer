77f4f599b0da2da6ed8163e66208948af60c2abc
robertwitt <robert_witt@icloud.com>
2021-04-06 19:36:54 +0200

    Refactor app and server

--- server/express/src/server.ts
+++ server/express/src/server.ts
@@ -1,7 +1,12 @@
-import { createApp } from "./app";
+import app from "./app";
 
-const app = createApp();
-
-app.listen(3000, () => {
-  console.log("Server started and is listening on port 3000");
+const server = app.listen(app.get("port"), () => {
+  console.log(
+    "App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  console.log("Press CTRL-C to stop\n");
 });
+
+export default server;
