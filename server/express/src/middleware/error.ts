36e42a9aaea934d4e6791233139296051fa1ad8e
robertwitt <robert_witt@icloud.com>
2021-04-06 20:06:24 +0200

    Refactor error handler

--- server/express/src/middleware/error.ts
+++ server/express/src/middleware/error.ts
@@ -1,20 +1,13 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 /**
  * Express handler for errors.
- * @param err error that occured
+ * @param err error that occurred
  * @param _req the request
  * @param res the response
  * @param _next pointer to next handler
  */
-export function handleError(
-  err: any,
-  _req: Request,
-  res: Response,
-  _next: NextFunction
-): void {
+export function handleError(err: Error, _req: Request, res: Response): void {
   console.error(err);
-  res
-    .status(err.status || 500)
-    .json({ message: err.message, errors: err.errors });
+  res.status(500).render("error", { error: err });
 }
