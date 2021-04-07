39790c4d91246686b3ba6997519c4ea17241c6f9
robertwitt <robert_witt@icloud.com>
2021-04-07 22:14:52 +0200

    Return copies from repository

--- server/express/src/model/codeList.ts
+++ server/express/src/model/codeList.ts
@@ -1,25 +1,20 @@
+/**
+ * Abstract implementation of a code list entity
+ */
 export default abstract class CodeList<Code, Name> {
-  private _code!: Code;
-  private _name?: Name;
+  private readonly _code: Code;
+  private readonly _name?: Name;
 
   constructor(code: Code, name?: Name) {
-    this.code = code;
-    this.name = name;
+    this._code = code;
+    this._name = name;
   }
 
   get code(): Code {
     return this._code;
   }
 
-  set code(value: Code) {
-    this._code = value;
-  }
-
   get name(): Name | undefined {
     return this._name;
-  }
-
-  set name(value: Name | undefined) {
-    this._name = value;
   }
 }
--- server/express/src/repository/mock/task.ts
+++ server/express/src/repository/mock/task.ts
@@ -10,6 +10,19 @@ export default class TaskMockRepository implements TaskRepository {
   }
 
   findById(id: TaskId): Promise<Task | undefined> {
-    return Promise.resolve(this.db.tasks.get(id));
+    const task = this.db.tasks.get(id);
+    return Promise.resolve(task ? this.copyTask(task) : undefined);
+  }
+
+  private copyTask(task: Task): Task {
+    return new Task(task.id, {
+      title: task.title,
+      collection: { ...task.collection },
+      dueDate: task.dueDate,
+      dueTime: task.dueTime,
+      status: task.status,
+      priority: task.priority,
+      isPlannedForMyDay: task.isPlannedForMyDay,
+    });
   }
 }
