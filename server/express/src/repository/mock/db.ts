ee0f5f8e81ab0a5703f3415802487e74e0343a62
robertwitt <robert_witt@icloud.com>
2021-04-07 21:48:53 +0200

    Add mock data

--- server/express/src/repository/mock/db.ts
+++ server/express/src/repository/mock/db.ts
@@ -27,6 +27,7 @@ export default class MockDb {
   private initialize(): void {
     const myTasks: TaskListRef = { id: 1, title: "My Tasks" };
     const openStatus = new TaskStatus("O", "open");
+    const doneStatus = new TaskStatus("D", "done");
     const lowPriority = new TaskPriority(5, "low");
     this.tasks.set(
       1,
@@ -36,6 +37,23 @@ export default class MockDb {
         dueDate: "2021-05-31",
         status: openStatus,
         priority: lowPriority,
+      })
+    );
+    this.tasks.set(
+      2,
+      new Task(2, {
+        title: "Groceries shopping",
+        collection: myTasks,
+        status: openStatus,
+        priority: lowPriority,
+      })
+    );
+    this.tasks.set(
+      42,
+      new Task(42, {
+        title: "What is the meaning of life?",
+        collection: { id: 42 },
+        status: doneStatus,
       })
     );
   }
