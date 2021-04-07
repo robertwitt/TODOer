ad23637bddd078cb55d33c724751a8cd3e72dff9
robertwitt <robert_witt@icloud.com>
2021-04-07 21:45:23 +0200

    Add integration tests

--- server/express/test/api/task.test.ts
+++ server/express/test/api/task.test.ts
@@ -12,4 +12,16 @@ describe("The API server", () => {
   it("cannot read a task by an unknown ID", async () => {
     await request.get("/beta/Tasks/99").expect(404);
   });
+
+  it("can read all tasks", async () => {
+    const { body } = await request.get("/beta/Tasks").expect(200);
+    expect(body).toEqual({
+      value: expect.arrayContaining([expect.objectContaining({ id: 1 })]),
+    });
+  });
+
+  it("can filter tasks by collection", async () => {
+    const { body } = await request.get("/beta/Tasks?collection=42").expect(200);
+    expect(body).toMatchSnapshot();
+  });
 });
