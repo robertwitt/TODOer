bad47c27749b18695d770ab64dd5151fefe7c828
Robert Witt <46402879+robertwitt@users.noreply.github.com>
2021-05-17 19:22:17 +0200

    feat(server-express): initialize mock DB (#47)

--- server/express/test/api/taskList.test.ts
+++ server/express/test/api/taskList.test.ts
@@ -1,11 +1,12 @@
 import supertest from "supertest";
 import app from "../../src/app";
 import MockDb from "../../src/repository/mock/db";
+import { testData } from "./testData";
 
 describe("The API server", () => {
   const request = supertest(app);
 
-  beforeEach(() => MockDb.instance.reset());
+  beforeEach(() => MockDb.instance.initializeWith(testData));
 
   it("can read a single task list by ID", async () => {
     const { body } = await request.get("/beta/TaskLists/1").expect(200);
