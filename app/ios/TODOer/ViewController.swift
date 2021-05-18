4f01c494f4067553f5dd1080d3cee7744c44bcc0
robertwitt <robert_witt@icloud.com>
2021-05-18 13:00:04 +0200

    Xcode project template

---
+++ app/ios/TODOer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODOer
+//
+//  Created by Witt, Robert on 18.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
